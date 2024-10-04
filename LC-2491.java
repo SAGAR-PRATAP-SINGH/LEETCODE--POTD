a19b0e077210fdb246397f053743fd8d464aa3a3
SAGAR PRATAP SINGH <101208192+SAGAR-PRATAP-SINGH@users.noreply.github.com>
2024-10-05 00:19:32 +0530

    Create LC-2491.java

---
+++ LC-2491.java
@@ -0,0 +1,37 @@
+class Solution {
+    public long dividePlayers(int[] skill) {
+        int n = skill.length;
+        int teams = n/2;
+        int totalsum =0;
+
+        //HashMap<Integer, Integer> freqMap = new HashMap<>();
+        int[] freqMap = new int[1001];
+        for(int element: skill){
+            freqMap[element]++;
+            //freqMap.put(element, freqMap.getOrDefault(element, 0)+1);
+            totalsum += element;
+        }
+        if(totalsum % teams != 0){
+            return -1;
+        }
+        int targetPoints = totalsum/teams;
+        long res = 0;
+        for(int element : skill){
+            //if(freqMap.get(element) == 0)
+            if(freqMap[element] == 0){
+                continue;
+            }
+            //freqMap.put(element, freqMap.get(element)-1);
+            freqMap[element]--;
+            int partner = targetPoints - element;
+            //if(!freqMap.containsKey(partner) || freqMap.get(partner) ==0)
+            if(freqMap[partner]==0){
+                return -1;
+            }
+            //freqMap.put(partner, freqMap.get(partner)-1);
+            freqMap[partner]--;
+            res = res + ((long)element * (long)partner);
+        }
+        return res;
+    }
+}
